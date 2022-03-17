46d276c1fac428ffb259add57f51d56002d110a5
Eduardo Turcios <eddieturcios@outlook.com>
2022-03-17 11:57:19 -0400

    Mapping Data

--- client/index.js
+++ client/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const contacts = [
+  { id: 1, name: 'R2-D2', phone: '222-222-2222', email: 'r2d2@droids.com' },
+  { id: 2, name: 'C-3PO', phone: '333-333-3333', email: 'c3po@droids.com' },
+  { id: 3, name: 'BB-8', phone: '888-888-8888', email: 'bb8@droids.com' },
+];
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = { contacts: contacts };
+  }
   render() {
     return (
       <div id="main">
@@ -9,14 +18,13 @@ class Main extends Component {
           <div>Contact List</div>
         </div>
         <div id="container">
-          {/* your components here */}
+          {this.state.contacts.map((contact, key) => (
+            <div>{contact.name}</div>
+          ))}
         </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(
-  <Main />,
-  document.getElementById('app')
-);
+ReactDOM.render(<Main />, document.getElementById('app'));
